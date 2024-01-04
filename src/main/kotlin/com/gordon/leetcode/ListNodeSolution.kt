d3227d47d9ee1ce5f82135eba3facbdd0b510f4d
gordon <gordon914feng@gmail.com>
2024-01-04 22:30:22 +0800

    新增 两两交换链表中的节点

--- src/main/kotlin/com/gordon/leetcode/ListNodeSolution.kt
+++ src/main/kotlin/com/gordon/leetcode/ListNodeSolution.kt
@@ -29,3 +29,19 @@ fun reverseList(head: ListNode?): ListNode? {
     }
     return pre
 }
+
+fun swapPairs(head: ListNode?): ListNode? {
+    val dummy = ListNode(-1)
+    dummy.next = head
+    var curr = dummy
+    while (curr.next != null && curr.next!!.next != null) {
+        val tmp = curr.next!!.next!!.next
+        var first = curr.next
+        var second = curr.next!!.next
+        curr.next = second
+        second?.next =first
+        first?.next = tmp
+        curr = first!!
+    }
+    return dummy.next
+}

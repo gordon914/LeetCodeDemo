29f8b146b6a07b78a6ce9fc44f5c28395c59248d
gordon <gordon914feng@gmail.com>
2024-01-05 22:46:22 +0800

    新增 解题思路-接雨水

--- src/main/kotlin/com/gordon/leetcode/Top100.kt
+++ src/main/kotlin/com/gordon/leetcode/Top100.kt
@@ -1,5 +1,11 @@
 package org.example.com.gordon.leetcode
 
+/**
+ * 接雨水,
+ * 左右指针,左指针指向最左边,右指针指向最右边. 那么所接的雨水最多是area = min(l,r)* (r-l)
+ * 然后移动值较小的那个指针, 每次记录计算的area值,保留最大的值.
+ * 当循环完时,返回记录的最大值
+ */
 fun maxArea(height: IntArray): Int {
     var l = 0
     var r = height.lastIndex

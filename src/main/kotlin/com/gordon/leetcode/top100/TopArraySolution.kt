cec5987558c4f9ebf0b69b078c77ffa674b1e154
gordon <gordon914feng@gmail.com>
2024-04-12 14:38:44 +0800

    新增 轮转数组

--- src/main/kotlin/com/gordon/leetcode/top100/TopArraySolution.kt
+++ src/main/kotlin/com/gordon/leetcode/top100/TopArraySolution.kt
@@ -47,3 +47,46 @@ fun merge(intervals: Array<IntArray>): Array<IntArray> {
     }
     return merged.toTypedArray()
 }
+
+/**
+ * 189. 轮转数组
+ * 给定一个整数数组 nums，将数组中的元素向右轮转 k 个位置，其中 k 是非负数。
+ * https://leetcode.cn/problems/rotate-array/description/?envType=study-plan-v2&envId=top-100-liked
+ *
+ * [1,2,3,4,5,6,7] --->[7,6,5,4,3,2,1] --->k =3
+ * [7,6,5,4,3,2,1] --->[5,6,7,4,3,2,1]
+ * [5,6,7,4,3,2,1] --->[5,6,7,1,2,3,4]
+ *
+ */
+fun rotate(nums: IntArray, k: Int): Unit {
+    val fk = k % nums.size
+    reverse(nums, 0, nums.lastIndex)
+    reverse(nums, 0, fk - 1)
+    reverse(nums, k, nums.lastIndex)
+}
+
+private fun reverse(nums: IntArray, left: Int, right: Int) {
+    var l = left
+    var r = right
+    while (l < r) {
+        val temp = nums[l]
+        nums[l] = nums[r]
+        nums[r] = temp
+        l++
+        r--
+    }
+}
+
+fun productExceptSelf(nums: IntArray): IntArray {
+    val ans = IntArray(nums.size)
+    ans[0] = 1
+    for (i in 1 until nums.size) {
+        ans[i] = ans[i - 1] * nums[i - 1]
+    }
+    var R = 1
+    for (i in nums.lastIndex downTo 0) {
+        ans[i] = ans[i] * R
+        R *= nums[i]
+    }
+    return ans
+}

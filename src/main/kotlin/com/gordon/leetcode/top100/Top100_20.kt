10140279df86b7ba07a34bcb012ac9dca0ede049
gordon <gordon914feng@gmail.com>
2024-04-16 20:52:37 +0800

    新增 top100-缺失的第一个正数

--- src/main/kotlin/com/gordon/leetcode/top100/Top100_20.kt
+++ src/main/kotlin/com/gordon/leetcode/top100/Top100_20.kt
@@ -692,3 +692,44 @@ fun productExceptSelf(nums: IntArray): IntArray {
     }
     return ans
 }
+
+/**
+ * 第17题 缺失的第一个正数
+ * 41. 缺失的第一个正数
+ * 给你一个未排序的整数数组 nums ，请你找出其中没有出现的最小的正整数。
+ *
+ * 请你实现时间复杂度为 O(n) 并且只使用常数级别额外空间的解决方案。
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [1,2,0]
+ * 输出：3
+ * 解释：范围 [1,2] 中的数字都在数组中。
+ * 示例 2：
+ *
+ * 输入：nums = [3,4,-1,1]
+ * 输出：2
+ * 解释：1 在数组中，但 2 没有。
+ * 示例 3：
+ *
+ * 输入：nums = [7,8,9,11,12]
+ * 输出：1
+ * 解释：最小的正数 1 没有出现。
+ */
+fun firstMissingPositive(nums: IntArray): Int {
+    val n = nums.size
+    for (i in nums.indices) {
+        while (nums[i] > 0 && nums[i] <= nums.size && nums[nums[i] - 1] != nums[i]) {
+            val tmp = nums[nums[i] - 1]
+            nums[nums[i] - 1] = nums[i]
+            nums[i] = tmp
+        }
+    }
+    for (i in nums.indices) {
+        if (nums[i] != i + 1) {
+            return i + 1
+        }
+    }
+    return n + 1
+}

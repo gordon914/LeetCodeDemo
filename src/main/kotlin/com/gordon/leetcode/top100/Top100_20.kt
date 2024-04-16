53cfc76dfd816e7d4bbad155dd47b81a1cd83a1d
gordon <gordon914feng@gmail.com>
2024-04-16 20:34:39 +0800

    新增 top100除自身以外数组的乘积

--- src/main/kotlin/com/gordon/leetcode/top100/Top100_20.kt
+++ src/main/kotlin/com/gordon/leetcode/top100/Top100_20.kt
@@ -656,3 +656,39 @@ private fun reverse(nums: IntArray, start: Int, end: Int) {
         r--
     }
 }
+
+/**
+ * 第16题 除自身以外数组的乘积
+ * 238. 除自身以外数组的乘积
+ * 给你一个整数数组 nums，返回 数组 answer ，其中 answer[i] 等于 nums 中除 nums[i] 之外其余各元素的乘积 。
+ *
+ * 题目数据 保证 数组 nums之中任意元素的全部前缀元素和后缀的乘积都在  32 位 整数范围内。
+ *
+ * 请 不要使用除法，且在 O(n) 时间复杂度内完成此题。
+ *
+ *
+ *
+ * 示例 1:
+ *
+ * 输入: nums = [1,2,3,4]
+ * 输出: [24,12,8,6]
+ * 示例 2:
+ *
+ * 输入: nums = [-1,1,0,-3,3]
+ * 输出: [0,0,9,0,0]
+ */
+fun productExceptSelf(nums: IntArray): IntArray {
+    val ans = IntArray(nums.size)
+    ans[0] = 1
+    //累乘当前元素左边的元素
+    for (i in 1 until nums.size) {
+        ans[i] = ans[i - 1] * nums[i - 1]
+    }
+    var factorR = 1
+    //再累乘当前元素右边的元素
+    for (i in nums.lastIndex downTo 0) {
+        ans[i] = ans[i] * factorR
+        factorR *= nums[i]
+    }
+    return ans
+}

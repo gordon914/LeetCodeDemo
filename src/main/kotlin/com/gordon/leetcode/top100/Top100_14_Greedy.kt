7625cfe9a55064207d77bfaf218cf4ea176228d0
gordon <gordon914feng@gmail.com>
2024-04-29 17:12:55 +0800

    新增 top100-贪心算法-划分字母区间

--- src/main/kotlin/com/gordon/leetcode/top100/Top100_14_Greedy.kt
+++ src/main/kotlin/com/gordon/leetcode/top100/Top100_14_Greedy.kt
@@ -16,8 +16,8 @@ fun maxProfit(prices: IntArray): Int {
     var profit = 0
     var lowestPrice = prices[0]
     for (i in 1 until prices.size) {
-        profit = maxOf(profit,prices[i]-lowestPrice)
-        lowestPrice = minOf(lowestPrice,prices[i])
+        profit = maxOf(profit, prices[i] - lowestPrice)
+        lowestPrice = minOf(lowestPrice, prices[i])
     }
     return profit
 }
@@ -37,15 +37,72 @@ fun maxProfit(prices: IntArray): Int {
 fun canJump(nums: IntArray): Boolean {
     var maxReach = 0
     for (i in nums.indices) {
+        //注意这一步不能少,提前判断是否可达
         // 如果最大的可达距离不能到达这个索引i,那么就不能完成
         if (i > maxReach) {
             return false
         }
-        maxReach = maxOf(i+nums[i],maxReach)
+        maxReach = maxOf(i + nums[i], maxReach)
         //如果提前得知能达到最后一个位置,就返回,避免冗余的迭代
         if (maxReach >= nums.lastIndex) {
             return true
         }
     }
     return true
+}
+
+/**
+ * 第79题 跳跃游戏II
+ * 跳跃游戏I,计算游戏结束需要跳跃的最小步数
+ */
+fun jump(nums: IntArray): Int {
+    var currDistance = 0
+    var nextReach = 0
+    var ans = 0
+    //只需遍历到倒数第二个位置即可,因为,假设当前刚好到达了倒数第二个位置,那么次数再加1就是到达最后一个位置了.
+    for (i in 0 until nums.size - 1) {
+        //记录历史可以跳跃的最大距离
+        nextReach = maxOf(nextReach, i + nums[i])
+        //如果当前所在的位置已经到达了当前所能跳跃的距离,就累加次数,同时更新当前所能跳跃的最大距离
+        if (i == currDistance) {
+            ans++
+            currDistance = nextReach
+        }
+    }
+    return ans
+}
+
+/**
+ * 第80题 划分字母区间
+ * 763. 划分字母区间
+ * 给你一个字符串 s 。我们要把这个字符串划分为尽可能多的片段，同一字母最多出现在一个片段中。
+ *
+ * 注意，划分结果需要满足：将所有划分结果按顺序连接，得到的字符串仍然是 s 。
+ *
+ * 返回一个表示每个字符串片段的长度的列表。
+ *
+ * 示例 1：
+ * 输入：s = "ababcbacadefegdehijhklij"
+ * 输出：[9,7,8]
+ * 解释：
+ * 划分结果为 "ababcbaca"、"defegde"、"hijhklij" 。
+ * 每个字母最多出现在一个片段中。
+ * 像 "ababcbacadefegde", "hijhklij" 这样的划分是错误的，因为划分的片段数较少。
+ */
+fun partitionLabels(s: String): List<Int> {
+    val arr = IntArray(26)
+    s.forEachIndexed { index, c ->
+        arr[c - 'a'] = index
+    }
+    var start = 0
+    var end = 0
+    val ans = mutableListOf<Int>()
+    for (i in s.indices) {
+        end = maxOf(end, arr[s[i] - 'a'])
+        if (i == end) {
+            ans.add(end - start + 1)
+            start = end + 1
+        }
+    }
+    return ans
 }
